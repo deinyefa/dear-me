d6e8a814327c5cc42789b65343a54ebdb908330c
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-09-06 12:24:59 -0400

    google authentication

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+// -- initial setup -- //
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
-});
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
 
+require('./routes/authRoutes')(app);
+
+// ---------------- PORT ------------- //
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+// -------------- ASSIGN COOKIE ------------- //
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+// -------------- DEASSIGN COOKIE ------------- //
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
+// -------------- PASSPORT ------------- //
+passport.use(
+	// ------------ GOOGLE AUTHENTICATION ------------- //
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			// ------------ MONGO DB  ------------- //
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				if (existingUser) {
+					done(null, existingUser);
+				} else {
+					new User({ googleId: profile.id })
+						.save()
+						.then(user => done(null, user));
+				}
+			});
+		}
+	)
+);
