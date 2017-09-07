d2f8cc8f5c441ddc2adfe1ce539e7ec4e5e7983c
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-09-07 08:53:40 -0400

    server side ajax request refactor

--- services/passport.js
+++ services/passport.js
@@ -24,17 +24,15 @@ passport.use(
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback'
 		},
-		(accessToken, refreshToken, profile, done) => {
+		async (accessToken, refreshToken, profile, done) => {
 			// ------------ MONGO DB  ------------- //
-			User.findOne({ googleId: profile.id }).then(existingUser => {
-				if (existingUser) {
-					done(null, existingUser);
-				} else {
-					new User({ googleId: profile.id })
-						.save()
-						.then(user => done(null, user));
-				}
-			});
+			const existingUser = await User.findOne({ googleId: profile.id });
+
+			if (existingUser) {
+				return done(null, existingUser);
+			}
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
 		}
 	)
 );
