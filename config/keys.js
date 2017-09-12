3c1c3302910800e410583c7339476b0fa881c098
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-09-12 14:57:21 -0400

    dev vs prod keys setup

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// ------------------- dev keys ------------------ //
+
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./prod');
+} else {
+	module.exports = require('./dev');
+}
