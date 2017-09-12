4cabda97938f42d035cbba55a20ee8457f0bf94e
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-09-12 15:10:17 -0400

    keys setup

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
+// const cookieSession = require('cookie-session');
+// const passport = require('passport');
 const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
+// require('./models/User');
+// require('./services/passport');
 const { Schema } = mongoose;
 const bodyParser = require('body-parser');
 // -- initial setup -- //
 mongoose.connect(keys.mongoURI);
 const app = express();
-require('./routes/authRoutes')(app);
-
-app.use(
-	cookieSession({
-		maxAge: 30 * 24 * 60 * 60 * 1000,
-		keys: [keys.cookieKey]
-	})
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// require('./routes/authRoutes')(app);
+//
+// app.use(
+// 	cookieSession({
+// 		maxAge: 30 * 24 * 60 * 60 * 1000,
+// 		keys: [keys.cookieKey]
+// 	})
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
